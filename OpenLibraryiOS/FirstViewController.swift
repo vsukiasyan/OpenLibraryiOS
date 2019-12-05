29ad5a6219b328f1f31aa61f1feee3a7fabc75f8
vsukiasyan <vsukiasyan1@hotmail.com>
2019-12-05 12:18:37 -0800

    - set up initial storyboards - started building out the search funcitonality and URL request

--- OpenLibraryiOS/FirstViewController.swift
+++ OpenLibraryiOS/FirstViewController.swift
@@ -8,11 +8,76 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
-
+class FirstViewController: UIViewController, UITableViewDataSource, UISearchResultsUpdating {
+    
+    @IBOutlet weak var tableView: UITableView!
+    var searchTask: DispatchWorkItem?
+    
+    var searchController: UISearchController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        
+        searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        
+        searchController.dimsBackgroundDuringPresentation = false
+        
+        searchController.searchBar.sizeToFit()
+        tableView.tableHeaderView = searchController.searchBar
+        
+        definesPresentationContext = true
+        
+    }
+    
+    
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        self.searchTask?.cancel()
+        
+        // Replace previous task with a new one
+        let task = DispatchWorkItem { [weak self] in
+            print(searchController.searchBar.text)
+            // Make URL request here
+            
+            guard let searchKey = searchController.searchBar.text else { return }
+            let url = "https://openlibrary.org/search.json?q=\(searchKey)"
+            guard let searchUrl = URL(string: url) else { return }
+            
+            print(searchUrl)
+            
+            URLSession.shared.dataTask(with: searchUrl, completionHandler: { (data, response, error) in
+                guard let data = data else { return }
+                do {
+                    let json = try JSONSerialization.jsonObject(with: data, options: [])
+                    print(json)
+                } catch {
+                    print("JSON error: \(error.localizedDescription)")
+                }
+                
+                
+            }).resume()
+            
+        }
+        self.searchTask = task
+        
+        // Execute task in 0.75 seconds (if not cancelled !)
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.75, execute: task)
+
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "TableCell") {
+            cell.textLabel?.text = "Test"
+            return cell
+        }
+        return UITableViewCell()
     }
 
 
---
+++ OpenLibraryiOS/SettingsViewController.swift
@@ -0,0 +1,37 @@
+//
+//  SettingsViewController.swift
+//  OpenLibraryiOS
+//
+//  Created by Vic Sukiasyan on 12/5/19.
+//  Copyright © 2019 Vic Sukiasyan. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func close(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
