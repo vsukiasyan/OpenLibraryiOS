2e350013a7a84c94074aa869aab9b7b1bfb9b302
vsukiasyan <vsukiasyan1@hotmail.com>
2019-12-05 10:24:54 -0800

    Initial Commit

---
+++ OpenLibraryiOS/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  OpenLibraryiOS
+//
+//  Created by Vic Sukiasyan on 12/5/19.
+//  Copyright © 2019 Vic Sukiasyan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
